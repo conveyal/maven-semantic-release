5f6ffcf58d38045adf382b29595ebe3cf1610b88
evansiroky <evan.siroky@yahoo.com>
2018-02-09 15:32:05 -0800

    feat(init): first commit
    copied over stuff from
    https://github.com/evansiroky/maven-semantic-release-example

---
+++ bin/release-cli.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+const semanticRelease = require('semantic-release/cli')
+
+/**
+ * Add necessary arguments to run semantic release for maven projects
+ */
+async function run () {
+  process.argv.push(...[
+    '--get-last-release',
+    'maven-semantic-release/lib/get-last-release',
+    '--publish',
+    'maven-semantic-release/lib/publish',
+    '--verify-conditions',
+    '@semantic-release/github,@semantic-release/condition-travis'
+  ])
+
+  await semanticRelease()
+}
+
+// allow invocation using node command
+if (require.main === module) {
+  run()
+}
+
+module.exports = run
