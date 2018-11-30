db2148c5cf39982f24fe279764ec45263b673352
evansiroky <evan.siroky@yahoo.com>
2018-11-29 17:34:08 -0800

    feat(verify-conditions): add verify-conditions step
    - also add some more logging
    - move some checks about the pom.xml into verify-conditions
    - check for existence of maven-settings.xml file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   prepare: require('./lib/prepare'),
   publish: require('./lib/publish'),
+  verifyConditions: require('./lib/verify-conditions'),
   verifyRelease: require('./lib/verify-release')
 }
